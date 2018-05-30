821ebc794e36683659d2420e50be2dc0dc9729bd
VincentJo <chiwoojo@gmail.com>
2018-05-29 22:38:36 -0700

    remove old code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-import _ from 'lodash';
+import sam from './sam';
 
-function component() {
-  var element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+sam.action('x');
